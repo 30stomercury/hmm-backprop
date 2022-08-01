e0708a5ad6078ef9bb6267e84dbb8f873190421e
Sung-Lin Yeh <sunglin.yeh@ed.ac.uk>
2022-08-01 14:41:27 +0100

    update

--- hmm_forward.cpp
+++ hmm_forward.cpp
@@ -14,9 +14,10 @@ Returns
 -------
 (B, N)
 */
-torch::Tensor log_matmul(torch::Tensor a, const torch::Tensor& b) {
+std::tuple<torch::Tensor, torch::Tensor> log_matmax(
+    torch::Tensor a, const torch::Tensor& b) {
     auto unsqueezed = a.unsqueeze(-2);
-    return torch::logsumexp(unsqueezed + b, /*dim=*/{-1}, false);
+    return torch::max(unsqueezed + b, /*dim=*/{-1}, false);
 }
 
 /* Sum operation in Log Semiring
@@ -29,14 +30,15 @@ log_b: (B, N, N)
 
 Returns
 -------
-(B, N)
+Tuple{(B, 1), (B, N)}
 */
-torch::Tensor log_matmul(torch::Tensor a, const torch::Tensor& b) {
+torch::Tensor log_matmul(
+    torch::Tensor a, const torch::Tensor& b) {
     auto unsqueezed = a.unsqueeze(-2);
     return torch::logsumexp(unsqueezed + b, /*dim=*/{-1}, false);
 }
 
-/* Voterbi algorithm 
+/* Viterbi algorithm (forward)
 
 Arguments
 ---------
@@ -46,28 +48,67 @@ Returns
 -------
 (B, T, N)
 */
-std::vector<torch::Tensor> hmm_viterbi(const torch::Tensor& potential) {
+std::vector<torch::Tensor> hmm_viterbi_forward(const torch::Tensor& potential) {
     const int batch = potential.sizes()[0];
     const int time = potential.sizes()[1];
     const int N = potential.sizes()[2];
     auto chart = torch::zeros({batch, time, N}, potential.device());
+    auto backptr = torch::zeros({batch, time, N, N}, potential.device());
     double logzero = -1e23;
+    std::tuple<torch::Tensor, torch::Tensor> scores;
     chart.index_put_({Slice(), 0}, potential.index({Slice(), 0, 0}));
     auto potential_ = potential.transpose(-2, -1);
     for (int64_t i = 1; i < time; ++i) {
-        chart.index({Slice(), i, Slice()}) = 
-            log_matmul(
+        scores = 
+            log_matmax(
                 chart.index({Slice(), i-1}), 
                 potential_.index({Slice(), i})
-            ).clamp(logzero, 0);
+            );
+        chart.index({Slice(), i}) = std::get<0>(scores).clamp(logzero, 0);
+        backptr.index({Slice(), i}).scatter_(
+            1, std::get<1>(scores).unsqueeze(-2), 1
+        );
     }
-    // (B, T)
-    auto partition = torch::logsumexp(chart, /*dim=*/{-1});
+    scores = torch::max(chart, /*dim=*/{-1});
+    auto partition = std::get<0>(scores);
+    auto terminal_states = std::get<1>(scores);
 
-    return {chart, partition};
+    return {backptr, terminal_states, partition};
 }
 
-/* Forward algorithm in log space.
+
+/* Backtracking algorithm (Backward).
+torch::Tensor hmm_viterbi_backward(
+        const torch::Tensor& grad_z, 
+        const torch::Tensor& backptr,
+        const torch::Tensor& terminal_states, // (B, 1)
+        const torch::Tensor& mask) {
+    const int batch = backptr.sizes()[0];
+    const int time = backptr.sizes()[1];
+    const int N = backptr.sizes()[2];
+    torch::Tensor chart = torch::zeros({batch, time, N, N}, potential.device());
+    chart += mask
+    
+    int64_t end = time - 1;
+    for (int64_t i = 0; i < end; ++i) {
+        chart.index_put_(
+            {Slice(), i+1},
+            fwd_vars_.index({Slice(), i}) + 
+            potential.index({Slice(), i+1}) +
+            bwd_vars_.index({Slice(), i+1})
+        );
+    }
+    torch::Tensor grad = 
+        chart.clamp(logzero, 0)
+        - partition.view({batch, 1, 1, 1})
+        + (-grad_z).log().view({batch, 1, 1, 1})
+        + mask;
+
+    return grad.exp();
+}
+*/
+
+/* Forward algorithm in log space (forward).
 
 Arguments
 ---------
@@ -139,6 +180,8 @@ torch::Tensor hmm_bw_forward(const torch::Tensor& potential) {
     return chart;
 }
 
+/* Forward-Backward algorithm in log space (Backward).
+*/
 torch::Tensor hmm_fw_backward(
         const torch::Tensor& grad_z, 
         const torch::Tensor& potential, 
@@ -189,4 +232,5 @@ torch::Tensor hmm_fw_backward(
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("forward", &hmm_fw_forward, "HMM forward");
   m.def("backward", &hmm_fw_backward, "HMM backward");
+  m.def("viterbi_forward", &hmm_viterbi_forward, "HMM viterbi forward");
 }
