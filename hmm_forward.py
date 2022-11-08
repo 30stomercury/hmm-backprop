fix wrong masking

hmm_forward.py
@@ -15,8 +15,8 @@ class HMMForward(Function):
             The unpadded are masked with 0 while the paddings are 
             masked with log zeros, e.g. [0, 0, 0, -1e23, -1e23].
         """
-        # mask the potential
-        potential = potential * mask_pad.exp()
+        # mask the potential for the backward algorithm
+        potential = potential + (-1 * mask_pad)
         chart, log_partition = hmm_forward_cpp.forward(potential)
         log_partition = torch.gather(log_partition, 1, lengths.view(-1, 1) - 1)
         ctx.save_for_backward(potential, chart, log_partition, mask_pad)